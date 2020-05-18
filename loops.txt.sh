748826da30fc2d3554c310dab5b616eda05f1210
jonatansmith <jonatansmith@gmail.com>
2020-05-18 10:45:02 -0300

    Add bucket copy loop

---
+++ loops.txt.sh
@@ -0,0 +1,13 @@
+
+#Author: jonatansmith@gmail.com
+#Date: 2020-05-18
+#Name: bucket copy
+#Description:
+#Copy files from one folder or bucket to another folder or bucket
+while read p; do
+  echo "$p";
+  aws s3 cp s3://s3bucket/2019-10-29/$p  s3://s3bucket/another_dir/2019-10-29/$p --profile awsconfigprofile --no-verify-ssl;
+  sleep 1;
+done <list_of_files_to_be_copied.txt
+#a text file with list of files to be copied. Can be obtained with:
+#ws s3 ls s3://s3bucket/2019-10-29/ --profile awsconfigprofile --no-verify-ssl >> list_of_files_to_be_copied;
